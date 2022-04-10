48f4784a24dfb6b173b84b4a4914a905405d6000
tyange <usun16@gmail.com>
2022-04-10 09:15:22 +0900

    시작 & 토큰 정의하기

---
+++ token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type tokenType string
+
+type Token struct {
+	Type    tokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 식별자 + 리터럴
+	IDENT = "IDENT" // add, foobar, x, y...
+	INT   = "INT"   //1345123
+
+	// 연산자
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// 구분자
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// 예약어
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
